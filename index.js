f0b89fd6939a8916001f3a23fc5f46f638c19557
Monika Gustilo <monikagustilo@gmail.com>
2022-07-17 17:13:27 -0700

    commit tree aad2659c96e6f123f2d9db952be5b4581bcac693

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((res) => res.json())
+    .then((data) => addNewContact(data.id))
+    .catch(() => {
+      let message = "Unauthorized Access";
+      document.body.append(message);
+    });
+}
+
+function addNewContact(newID) {
+  document.getElementsByTagName("body")[0].innerHTML = newID;
+}
+
+// submitData("monika", "monika@email.com");
